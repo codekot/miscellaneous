ddea0a6c2917f3e8ad7d68a2e02bac714ea59a79
codekot <boobookish@gmail.com>
2020-11-24 15:27:15 +0400

    add color coding last eruption

--- Volcano_map/main.py
+++ Volcano_map/main.py
@@ -13,11 +13,28 @@ volcanoes_coord = []
 
 feature_group = folium.FeatureGroup(name="My Map")
 
+def color_code(code):
+    if code == "D1":
+        return "red"
+    elif code == "D2":
+        return "orange"
+    elif code == "D3":
+        return "green"
+    elif code == "D4":
+        return "lightblue"
+    elif code == "D5":
+        return "blue"
+    elif code == "D6":
+        return "darkblue"
+    else:
+        return "gray"
+
 for row in df.values:
     location = [row[4], row[5]]
     volcano_name = row[1]
+    color = color_code(row[9])
     feature_group.add_child(folium.Marker(location=location, popup=volcano_name,
-                            icon=folium.Icon(color="green")))
+                            icon=folium.Icon(color=color)))
 
 map.add_child(feature_group)
 map.save("Map1.html")
