346cdc05fed67133d02d6ffa2f2178003b10117b
codekot <boobookish@gmail.com>
2020-11-24 15:30:20 +0400

    add comment about date of eruption

--- Volcano_map/main.py
+++ Volcano_map/main.py
@@ -14,6 +14,17 @@ volcanoes_coord = []
 feature_group = folium.FeatureGroup(name="My Map")
 
 def color_code(code):
+    # The date of the last known eruption (from the Smithsonian Institution, Global Volcanism Program)
+    # D1	Last known eruption 1964 or later
+    # D2	Last known eruption 1900-1963
+    # D3	Last known eruption 1800-1899
+    # D4	Last known eruption 1700-1799
+    # D5	Last known eruption 1500-1699
+    # D6	Last known eruption A.D. 1-1499
+    # D7	Last known eruption B.C. (Holocene)
+    # U	Undated, but probable Holocene eruption
+    # Q	Quaternary eruption(s) with the only known Holocene activity being hydrothermal
+    # ?	Uncertain Holocene eruption
     if code == "D1":
         return "red"
     elif code == "D2":
