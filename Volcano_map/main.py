7a661e2819004474d91c0e4642e7dcd0b46f3e85
codekot <boobookish@gmail.com>
2020-11-24 15:09:17 +0400

    add volcano name to marker

--- Volcano_map/main.py
+++ Volcano_map/main.py
@@ -10,13 +10,13 @@ print(df.head())
 # print(df.iloc[1,4:6])
 
 volcanoes_coord = []
-for row in df.values:
-    volcanoes_coord.append([row[4], row[5]])
 
 feature_group = folium.FeatureGroup(name="My Map")
 
-for volcano in volcanoes_coord:
-    feature_group.add_child(folium.Marker(location=volcano, popup="New Marker",
+for row in df.values:
+    location = [row[4], row[5]]
+    volcano_name = row[1]
+    feature_group.add_child(folium.Marker(location=location, popup=volcano_name,
                             icon=folium.Icon(color="green")))
 
 map.add_child(feature_group)
