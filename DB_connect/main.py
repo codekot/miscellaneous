dac701a727a3135a652fef2fe2a5f6a67c6c5c67
codekot <boobookish@Gmail.com>
2020-11-28 08:27:43 +0400

    add delete and update functions

--- DB_connect/main.py
+++ DB_connect/main.py
@@ -1,30 +1,48 @@
 import sqlite3
 
+DATABASE = "lite.db"
+
 def create_table():
-    connection = sqlite3.connect("lite.db")
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)
     """)
-    cursor.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.5)")
     connection.commit()
     connection.close()
 
 def insert(item, quantity, price):
-    connection = sqlite3.connect("lite.db")
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     cursor.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
     connection.commit()
     connection.close()
 
 def view():
-    connection = sqlite3.connect("lite.db")
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM store")
     rows = cursor.fetchall()
     connection.close()
     return rows
+    
+def delete(item):
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM store WHERE item=?", (item,))
+    connection.commit()
+    connection.close()
+    
+def update(item, quantity, price):
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price,item))
+    connection.commit()
+    connection.close()
 
-
-insert("Coffee Cup", 10, 5)
+create_table()
+#insert("Coffee Cup", 10, 5)
+#insert('Wine Glass', 8, 10.5)
+delete("Coffee Cup")
+update("Wine Glass", 10, 15)
 print(view())
