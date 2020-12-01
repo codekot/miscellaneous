242d07c693f34c28d81d385d2138b689a0c9e57f
codekot <boobookish@gmail.com>
2020-12-01 16:47:50 +0400

    update insert function to work with postgres

--- DB_connect/main.py
+++ DB_connect/main.py
@@ -3,12 +3,14 @@ from database_credentials import *
 
 DATABASE = "database1"
 
+
 def create_table():
     """
     function creating table in database
     """
     connection = psycopg2.connect(
-        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT)
+        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
     cursor = connection.cursor()
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)
@@ -16,38 +18,53 @@ def create_table():
     connection.commit()
     connection.close()
 
+
 def insert(item, quantity, price):
-    connection = psycopg2.connect(DATABASE)
+    connection = psycopg2.connect(
+        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
+    cursor.execute("INSERT INTO store VALUES (%s,%s,%s)",
+                   (item, quantity, price))
     connection.commit()
     connection.close()
 
+
 def view():
-    connection = psycopg2.connect(DATABASE)
+    connection = psycopg2.connect(
+        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM store")
     rows = cursor.fetchall()
     connection.close()
     return rows
-    
+
+
 def delete(item):
-    connection = psycopg2.connect(DATABASE)
+    connection = psycopg2.connect(
+        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
     cursor = connection.cursor()
     cursor.execute("DELETE FROM store WHERE item=?", (item,))
     connection.commit()
     connection.close()
-    
+
+
 def update(item, quantity, price):
-    connection = psycopg2.connect(DATABASE)
+    connection = psycopg2.connect(
+        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
     cursor = connection.cursor()
-    cursor.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price,item))
+    cursor.execute("UPDATE store SET quantity=?, price=? WHERE item=?",
+                   (quantity, price, item))
     connection.commit()
     connection.close()
 
+
 create_table()
-#insert("Coffee Cup", 10, 5)
-#insert('Wine Glass', 8, 10.5)
-#delete("Coffee Cup")
-#update("Wine Glass", 10, 15)
-#print(view())
+insert("Coffee Cup", 10, 5)
+# insert('Wine Glass', 8, 10.5)
+# delete("Coffee Cup")
+# update("Wine Glass", 10, 15)
+# print(view())
