761065fc8f6302d83e63adbacfb2b374e76cc633
codekot <boobookish@gmail.com>
2020-12-25 17:07:58 +0400

    Set a text menu

--- movies/models.py
+++ movies/models.py
@@ -46,6 +46,12 @@ class User:
     def watched_movies(self):
         return list(filter(lambda movie: movie.watched, self.movies))
 
+    def set_watched(self, name):
+        for movie in self.movies:
+            if movie.name == name:
+                movie.watched = True
+                break
+
     def to_json(self):
         return {
             "name": self.name,
