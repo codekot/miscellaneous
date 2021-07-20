288f5ca2132e99cb7747760913339fab8b07aa1b
codekot <boobookish@gmail.com>
2021-07-20 15:35:42 +0400

    Add generations

--- GA_array/main.php
+++ GA_array/main.php
@@ -7,12 +7,13 @@ $IND_NUMBER = 0;
 $BORDER = 4;
 $MUTATION_RATE = 2;
 $FITTEST_QUOTE = 4;
-$EVOLUTION_CICLES = 1000;
+$EVOLUTION_CYCLES = 2;
 $CURRENT_POPULATION = [];
 
 
 function fitness($individual){
     global $GOAL;
+    print_individual($individual);
     return array_sum($individual)/array_sum($GOAL);
 }
 
@@ -34,14 +35,29 @@ function generate_population($population = [], $quantity = 10){
 }
 
 function choose_fittest($population){
-    echo "hello";
-    foreach($population as $number => &$individual) {
+    print_population($population);
+    foreach($population as &$individual) {
         $individual[0] = fitness($individual[2]);
         echo $individual[0];
     }
     unset($individual);
     arsort($population);
+
     return $population;
+}
+
+function print_individual($individual){
+    foreach ($individual as $item) {
+        echo "[" . $item . "]";
+    }
+    echo "\n";
+}
+
+function print_population($population){
+    echo "POPULATION";
+    foreach($population as $individual){
+        print_individual($individual);
+    }
 }
 
 function cut_weakest($population){
@@ -57,16 +73,16 @@ function mutate_population($population){
     // take first four of population and mutate it and fill the second half
     // fifth remain
     // last is random
-    $i = 0;
     for ($i=0; $i<$FITTEST_QUOTE; $i++){
         $population[$i+5] = [0.0, $IND_NUMBER, mutate_individual($population[$i][3])];
         $IND_NUMBER++;
     }
     $population[9] = [0.0, $IND_NUMBER, generate_individual()];
+    return $population;
 }
 
 function random_with_probability($p=80){
-    $ans = rand(range(1,100));
+    $ans = rand(1,100);
     if($ans<$p){
         return true;
     } else {
@@ -78,12 +94,12 @@ function random_with_probability($p=80){
 function mutate_individual($individual){
     global $MUTATION_RATE;
     // choose how many mutation
-    $mutation_quantity = rand(range(1, $MUTATION_RATE));
+    $mutation_quantity = rand(1, $MUTATION_RATE);
 
     // choose which genes will be mutating
     $index_array = [];
     for($i=0; $i<$mutation_quantity; $i++){
-        $index_value = rand(range(0, 9));
+        $index_value = rand(0, 9);
         if(!array_search($index_value, $index_array)) {
             $index_array[] = $index_value;
         }
@@ -103,25 +119,35 @@ function mutate_individual($individual){
 }
 
 function evolution_step($population){
-    $f = choose_fittest($population);
-    // $f = fitness([0,1,0,1,0,1,0,1,0,0]);
-    //echo var_dump($f);
-    echo var_dump($f[0]);
+    global $CURRENT_POPULATION;
+    $population = choose_fittest($population);
+    $population = mutate_population($population);
+    $CURRENT_POPULATION = $population;
+    return $population;
+}
+
+function goal_achieved($population){
+    if ($population[0][0] >= 1.0){
+        return true;
+    }  else {
+        return false;
+    }
 }
 
 function main(){
-    global $EVOLUTION_CICLES;
+    global $EVOLUTION_CYCLES;
     global $CURRENT_POPULATION;
     $population = generate_population();
-    for($i=0; $i<$EVOLUTION_CICLES; $i++){
-        $result = evolution_step();
-        if($result){
-            echo $result;
-        }
+    $i = 0;
+    while($i<$EVOLUTION_CYCLES || !goal_achieved($population)){
+        $population = evolution_step($population);
+        $i++;
     }
+
     echo "End cicles";
     echo "Final population:";
     echo var_dump($CURRENT_POPULATION);
+    echo var_dump($population);
 }
 
 main();
