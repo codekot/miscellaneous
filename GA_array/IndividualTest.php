b864aad9110bf59e480e8c452e40d38edf5fb240
codekot <boobookish@gmail.com>
2021-08-16 17:19:14 +0400

    Add more assertions for individual

--- GA_array/IndividualTest.php
+++ GA_array/IndividualTest.php
@@ -12,9 +12,9 @@ class IndividualTest extends \PHPUnit\Framework\TestCase {
             $i
         );
         //object has proper attributes
-        $this -> assertObjectHasAttribute("array", $i);
-        $this -> assertObjectHasAttribute("fitness", $i);
-        $this -> assertObjectHasAttribute("personal_number", $i);
+        $this -> assertObjectHasAttribute("array", $i, "Individual has no array attribute");
+        $this -> assertObjectHasAttribute("fitness", $i, "Individual has no fitness attribute");
+        $this -> assertObjectHasAttribute("personal_number", $i, "Individual has no personal number");
 
         //attributes of proper types
         $this -> assertIsArray($i->array);
@@ -22,8 +22,15 @@ class IndividualTest extends \PHPUnit\Framework\TestCase {
         $this -> assertIsInt($i->personal_number);
 
         //attribute values within proper range
-        $this -> assertContainsOnly([0, 1], $i->array);
+        $this -> assertContainsOnly("int", $i->array);
+        $this -> assertContains(0, $i->array);
+        $this -> assertContains(1, $i->array);
+        $this -> assertNotContains(3, $i->array);
 
+        global $INDIVIDUAL_LENGTH;
+        $this -> assertEquals($INDIVIDUAL_LENGTH, count($i->array));
 
+        $this -> assertLessThanOrEqual(1.0, $i->fitness);
+        $this -> assertGreaterThanOrEqual(0.0, $i->fitness);
     }
 }
