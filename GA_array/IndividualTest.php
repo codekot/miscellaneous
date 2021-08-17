91bdc54f82396dbcc41d2b465e4b1c87f7a28eb3
codekot <boobookish@gmail.com>
2021-08-17 16:56:38 +0400

    Add assertions for clone_individual

--- GA_array/IndividualTest.php
+++ GA_array/IndividualTest.php
@@ -26,11 +26,33 @@ class IndividualTest extends \PHPUnit\Framework\TestCase {
         $this -> assertContains(0, $i->array);
         $this -> assertContains(1, $i->array);
         $this -> assertNotContains(3, $i->array);
-      
+
         global $INDIVIDUAL_LENGTH;
         $this -> assertEquals($INDIVIDUAL_LENGTH, count($i->array));
 
         $this -> assertLessThanOrEqual(1.0, $i->fitness);
         $this -> assertGreaterThanOrEqual(0.0, $i->fitness);
     }
+
+    public function testGetFitness(){
+        $i = new Individual();
+        $this -> assertIsFloat($i->get_fitness());
+        global $GOAL;
+        $i->array = $GOAL;
+        $this -> assertEquals(1.0, $i->get_fitness());
+    }
+
+    public function testCloneIndividual(){
+        $i = new Individual();
+        $i_clone = $i->clone_individual();
+        $this -> assertInstanceOf(
+            Individual::class,
+            $i_clone
+        );
+        $this -> assertNotEquals($i, $i_clone);
+        $this -> assertEquals($i->fitness, $i_clone->fitness);
+        $this -> assertEquals($i->array, $i_clone->array);
+        $this -> assertEquals($i->personal_number + 1, $i_clone->personal_number);
+    }
+
 }
